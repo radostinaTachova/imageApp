06aec03a8c11baf8bb5f4b8d9679fc284bdabd25
radostina <radostina.tachova@gmail.com>
2023-11-09 13:17:53 +0100

    added imagepicker for ios<16, moved viewmodels to ui layer

--- ImageApp/UI/ContentView.swift
+++ ImageApp/UI/ContentView.swift
@@ -16,20 +16,37 @@ struct ContentView: View {
     //MARK: temporal
     let url = "https://api.imgur.com/oauth2/authorize?client_id=3a6a531dc50e73b&response_type=token&state=APPLICATION_STATE"
 
+    //TODO: temporal, make a imagepicker wrapper view
+    @State private var image = UIImage()
+    @State private var showSheet = false
+    @State private var source: UIImagePickerController.SourceType = .photoLibrary
     
     var body: some View {
         //TODO: nevigation
         VStack {
             getLoginOrGallery
-            Spacer()
-            
-            Button("Log out", action: {
-                loginViewModel.logOut()
-            })
-            
         }.onAppear {
             if loginViewModel.isLoggedIn {
                 viewModel.getImages()
+            }
+        }.sheet(isPresented: $showSheet, content: {
+            ImagePicker(sourceType: $source, selectedImage: self.$image)
+        })
+    }
+    
+    var buttons: some View {
+        HStack {
+            Button("Importar Imagen") {
+                print("RTC = importar imagen")
+                source = .photoLibrary
+                showSheet = true
+            }
+            Spacer()
+            Button("Cámara") {
+                print("RTC = camera")
+                source = .camera
+                showSheet = true
+                
             }
         }
     }
@@ -44,13 +61,18 @@ struct ContentView: View {
                 loginViewModel.saveAccount(loginURL)
             }.ignoresSafeArea()
         default:
-            GalleryView(images: viewModel.images)
+            VStack {
+                GalleryView(images: viewModel.images)
+                Button("Log out", action: {
+                    loginViewModel.logOut()
+                })
+                buttons.padding()
+            }
         }
     }
-    
-    
-    
 }
+
+
 
 #Preview {
     ContentView(viewModel: ImageViewModel(ImgurRepository()), loginViewModel: LoginViewModel(LoginRepositoryImpl()))
