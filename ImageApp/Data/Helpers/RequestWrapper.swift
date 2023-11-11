59a813143257e5c1d278db742fe3e9a5d8140d86
radostina <radostina.tachova@gmail.com>
2023-11-11 11:49:34 +0100

    added request wrapper, added helpers group

---
+++ ImageApp/Data/Helpers/RequestWrapper.swift
@@ -0,0 +1,46 @@
+//
+//  RequestWrapper.swift
+//  ImageApp
+//
+//  Created by Radostina Tachova Chergarska on 11/11/23.
+//
+
+import Foundation
+
+struct RequestWrapper {
+    private var request: URLRequest
+    
+    init(url: String, withAccessToken auth: Bool) {
+        self.request = URLRequest(url: URL(string: url)!) //TODO: remove !
+        
+        if auth {
+            guard let accessTokenData = KeyChainHelper.retrieve(service: "access_token") else {
+                return
+            }
+            guard let accessToken = String(data: accessTokenData, encoding: .utf8) else {
+                return
+            }
+            
+            self.request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        }
+    }
+    
+    func get() -> URLRequest {
+        return request
+    }
+    
+    
+    mutating func addBodyForm(with name: String, value: String) {
+        let boundary = "Boundary-\(UUID().uuidString)"
+        request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        var body = ""
+        body += "--\(boundary)\r\n"
+        body += "Content-Disposition:form-data; name=\"\(name)\""
+        body += "\r\n\r\n\(value)\r\n"
+        body += "--\(boundary)--\r\n"
+        let postData = body.data(using: .utf8)
+        request.httpBody = postData
+    }
+    
+}
--- ImageApp/Data/Repositories/ImgurRepository.swift
+++ ImageApp/Data/Repositories/ImgurRepository.swift
@@ -17,13 +17,6 @@ struct ImgurRepository: ImageRepository {
     func getImages() -> AnyPublisher<[ImageUIItem], Error> {
         
         //TODO: maybe a auth class / api calls class / CredentialsManager (?¿)
-        guard let accessTokenData = KeyChainHelper.retrieve(service: "access_token") else {
-            return AnyPublisher(Fail<[ImageUIItem], Error>(error: URLError(URLError.Code.unknown))) //TODO: handle the errors or change the flow
-        }
-        guard let accessToken = String(data: accessTokenData, encoding: .utf8) else {
-            return AnyPublisher(Fail<[ImageUIItem], Error>(error: URLError(URLError.Code.unknown))) //TODO: handle the errors or change the flow
-        }
-        
         guard let userNameData = KeyChainHelper.retrieve(service: "userName") else {
             return AnyPublisher(Fail<[ImageUIItem], Error>(error: URLError(URLError.Code.unknown))) //TODO: handle the errors or change the flow
         }
@@ -31,16 +24,12 @@ struct ImgurRepository: ImageRepository {
         guard let userName = String(data: userNameData, encoding: .utf8) else {
             return AnyPublisher(Fail<[ImageUIItem], Error>(error: URLError(URLError.Code.unknown))) //TODO: handle the errors or change the flow
         }
-        //
-        
         print("RTC = getImages with userName = \(userName)")
         
-        let url = baseURL + "account/\(userName)/images" //TODO: remove user
-        var request = URLRequest(url: URL(string: url)!)
-        request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
-        //
-       
-        return session.dataTaskPublisher(for: request)
+        let url = baseURL + "account/\(userName)/images"
+        var request = RequestWrapper(url: url, withAccessToken: true)
+
+        return session.dataTaskPublisher(for: request.get())
             .tryMap { data, response in
                 //TODO: handle the errors
                 let images = try? JSONDecoder().decode(Result<[ImageItem]>.self, from: data)
@@ -53,33 +42,10 @@ struct ImgurRepository: ImageRepository {
     
     func uploadImage(base64: String) -> AnyPublisher<ImageUIItem, Error>  {
         let url = baseURL + "image"
-        
-        var request = URLRequest(url: URL(string: url)!)
-        //
-        //TODO: maybe a auth class / api calls class / CredentialsManager (?¿)
-        guard let accessTokenData = KeyChainHelper.retrieve(service: "access_token") else {
-            return AnyPublisher(Fail<ImageUIItem, Error>(error: URLError(URLError.Code.unknown))) //TODO: handle the errors or change the flow
-        }
-        guard let accessToken = String(data: accessTokenData, encoding: .utf8) else {
-            return AnyPublisher(Fail<ImageUIItem, Error>(error: URLError(URLError.Code.unknown))) //TODO: handle the errors or change the flow
-        }
-        
-        //TODO: API CALL CLASS WRAPPER
-        let boundary = "Boundary-\(UUID().uuidString)"
-        request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
-
-        request.addValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = "POST"
-        var body = ""
-        body += "--\(boundary)\r\n"
-        body += "Content-Disposition:form-data; name=\"image\""
-        body += "\r\n\r\n\(base64)\r\n"
-        body += "--\(boundary)--\r\n"
-        let postData = body.data(using: .utf8)
-        request.httpBody = postData
-        
-        
-        return session.dataTaskPublisher(for: request)
+        var request = RequestWrapper(url: url, withAccessToken: true)
+        request.addBodyForm(with: "image", value: base64)
+  
+        return session.dataTaskPublisher(for: request.get())
             .tryMap { data, response in
                 //TODO: handle errors
                 let result = try? JSONDecoder().decode(Result<ImageItem>.self, from: data)
@@ -88,10 +54,6 @@ struct ImgurRepository: ImageRepository {
             }
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
-        
-        
-        
     }
  
-    
 }
